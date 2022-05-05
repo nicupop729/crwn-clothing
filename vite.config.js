ec56d334718a253b81d6f2132426441578ffd872
Nicolae Pop <nicupop729@gmail.com>
2022-05-05 19:17:43 -0400

    Add different changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });
